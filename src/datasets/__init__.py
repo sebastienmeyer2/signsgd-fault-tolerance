12b34be65a8087c455b994bd97cb519e333b45d0
Sébastien Meyer <86206717+sebastienmeyer2@users.noreply.github.com>
2022-01-22 15:40:50 +0100

    Delete src directory

--- src/datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Contain datasets and partitions classes."""
-
-
-from datasets.partition_generator import Partition, PartitionGenerator
-
-
-__all__ = ["Partition", "PartitionGenerator"]
--- src/nn/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Contains various neural networks adapted to specific datasets."""
-
-
-from nn.mnist_nets import TorchNet, MNISTNet
-from nn.reg_nets import LogRegNet, LinRegNet
-from nn.resnets import ResNet18, ResNet50
-
-
-__all__ = ["LogRegNet", "LinRegNet", "TorchNet", "MNISTNet", "ResNet18", "ResNet50"]
